ddd830049e90ce6763918a3c8ac08742e9ac7bf6
sagilityph <sagilityassessment@gmail.com>
2026-06-30 14:52:53 +0800

    Update script.js

--- script.js
+++ script.js
@@ -9,5 +9,5 @@ function showPDF(file) {
     window.scrollTo({ top: pdfContainer.offsetTop, behavior: 'smooth' });
 }
 
-level1Btn.addEventListener('click', () => showPDF('Level1.pdf'));
-level2Btn.addEventListener('click', () => showPDF('Level2.pdf'));
+level1Btn.addEventListener('click', () => showPDF('level1.pdf'));
+level2Btn.addEventListener('click', () => showPDF('level2.pdf'));
